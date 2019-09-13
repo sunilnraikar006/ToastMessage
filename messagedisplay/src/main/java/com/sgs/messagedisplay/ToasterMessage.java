225be44d9b416d57604006e1e38ef60d05b6ba2a
sunil.n.raikar <sunil.n.raikar@shrigowri.com>
2019-09-13 16:10:11 +0530

    intial commit

---
+++ messagedisplay/src/main/java/com/sgs/messagedisplay/ToasterMessage.java
@@ -0,0 +1,17 @@
+package com.sgs.messagedisplay;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by sunilnraikar on 13/09/19.
+ */
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
