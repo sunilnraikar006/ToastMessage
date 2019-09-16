243900259f4e3dc307401c9501ecf05577473337
sunil.n.raikar <sunil.n.raikar@shrigowri.com>
2019-09-16 13:13:42 +0530

    added new access specifier

--- app/src/main/java/com/sgs/app/MainActivity.java
+++ app/src/main/java/com/sgs/app/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.sgs.messagedisplay.ToasterMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToasterMessage.s(this,"Hi, Hello , How Are You....");
     }
 }
--- messagedisplay/src/main/java/com/sgs/messagedisplay/ToasterMessage.java
+++ messagedisplay/src/main/java/com/sgs/messagedisplay/ToasterMessage.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message){
+    private static void s(Context c, String message){
 
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
 
