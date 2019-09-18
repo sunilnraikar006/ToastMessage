c8a49b7eda3ae85430825d632da233317357b2ad
sunil.n.raikar <sunil.n.raikar@shrigowri.com>
2019-09-18 13:17:13 +0530

    added final method

--- messagedisplay/src/main/java/com/sgs/messagedisplay/ToasterMessage.java
+++ messagedisplay/src/main/java/com/sgs/messagedisplay/ToasterMessage.java
@@ -7,9 +7,9 @@ import android.widget.Toast;
  * Created by sunilnraikar on 13/09/19.
  */
 
-public final class  ToasterMessage {
+public  class  ToasterMessage {
 
-    private static void s(Context c, String message){
+    private static final void s(Context c, String message){
 
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
 
